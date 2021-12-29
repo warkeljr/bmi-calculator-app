834b311c40ede66a32220a0bc9be817fe345da12
Warkel <warkel@me.com>
2021-12-29 19:14:31 +0100

    USER model null value error fixed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,7 @@
 class UserMod {
   UserMod({ this.uid, this.email, this.name, this.isAnonymous });
-  final String uid;
-  final String email;
-  final String name;
-  final bool isAnonymous;
+  final String? uid;
+  final String? email;
+  final String? name;
+  final bool? isAnonymous;
 }
