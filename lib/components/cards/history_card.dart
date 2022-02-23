9f447c49c79fb7b821bdf1a00af31b1d4fd6a6ab
warkeljr <warkel@me.com>
2022-02-23 09:51:06 +0100

    Changed back the history card en the calculatorbrain back to te new ones

--- lib/components/cards/history_card.dart
+++ lib/components/cards/history_card.dart
@@ -15,55 +15,98 @@ class BmiHistoryCard extends StatelessWidget {
     return Card(
       color: kActiveCardColor,
       margin: EdgeInsets.all(10.0),
-      child: Padding(padding: EdgeInsets.all(0.0),
-      child: Slidable(
-        key: ValueKey(0),
-        endActionPane: ActionPane(
-          motion: DrawerMotion(),
-          dismissible: DismissiblePane(onDismissed: () {}),
-          children: [
-             SlidableAction(
-      onPressed: null,
-      backgroundColor: Color(0xFFFE4A49),
-      foregroundColor: Colors.white,
-      icon: Icons.delete,
-      label: 'Delete',
-    ),
-          ],
-        ),
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
+      child: Padding(
+        padding: EdgeInsets.all(0.0),
+        child: Slidable(
+          key: ValueKey(0),
+          endActionPane: ActionPane(
+            motion: DrawerMotion(),
+            dismissible: DismissiblePane(onDismissed: () {}),
             children: [
-              Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(bottom: 0.0)),
-                  Text('${_bmi.bmiResultText}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
-                  SizedBox(
-                        width: SizeConfig.blockSizeHorizontal! * 5,
-                      ),
-                  Text('${_bmi.bmiResult}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: kBmiRed),),
-                ],
-              ),
-              SizedBox(
-                        height: SizeConfig.blockSizeVertical! * 2,
-                      ),
-              Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(bottom: 0.0)),
-                  Flexible(
-                    child: Text('${_bmi.bmiInterpretation}',
-                    maxLines: 2,
-                    overflow: TextOverflow.visible,
-                    softWrap: true,),
-                  ),
-                ],
+              SlidableAction(
+                onPressed: null,
+                backgroundColor: Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
               ),
             ],
           ),
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.only(bottom: 0.0)),
+                    Text(
+                      '${_bmi.bmiResultText}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                          color: bmicolor()),
+                    ),
+                    SizedBox(
+                      width: SizeConfig.blockSizeHorizontal! * 5,
+                    ),
+                    Flexible(
+                      child: Text(
+                        '${_bmi.bmiResult}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: bmicolor()),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical! * 1,
+                ),
+                Divider(
+                  color: Colors.grey,
+                ),
+                Row(
+                  children: [
+                    Padding(padding: EdgeInsets.only(bottom: 0.0)),
+                    Flexible(
+                      child: Text(
+                        '${_bmi.bmiInterpretation}',
+                        maxLines: 2,
+                        overflow: TextOverflow.visible,
+                        softWrap: true,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
       ),
     );
   }
+
+  Color? bmicolor() {
+    if (_bmi.bmiResult == 'Very serious underweight') {
+      return kBmiBlueDark;
+    } else if (_bmi.bmiResult == 'Severely underweight') {
+      return kBmiBlueMiddle;
+    } else if (_bmi.bmiResult == 'Underweight') {
+      return kBmiBlueLight;
+    } else if (_bmi.bmiResult == 'Normal') {
+      return kBmiGreenColor;
+    } else if (_bmi.bmiResult == 'Overweight') {
+      return kBmiYellow;
+    } else if (_bmi.bmiResult == 'Obesity class 1') {
+      return kBmiOrangeLight;
+    } else if (_bmi.bmiResult == 'Obesity class 2') {
+      return kBmiOrangeDark;
+    } else if (_bmi.bmiResult == 'Obesity class 3') {
+      return kBmiRed;
+    } else {
+      return kWhiteColor;
+    }
+  }
 }
+
--- lib/models/calculator_brain.dart
+++ lib/models/calculator_brain.dart
@@ -14,26 +14,43 @@ class CalculatorBrain {
   }
 
   String getResult() {
-    if (_bmi > 18.5 && _bmi <= 24.9) {
-      return 'Normal';
-    } else if (_bmi > 24.9 && _bmi <= 29.9) {
-      return 'Slightly overweight';
-    } else if (_bmi > 29.9) {
-      return 'Heavily overweight';
-    } else {
+    if (_bmi < 16) {
+      return 'Very serious underweight';
+    } else if (_bmi >= 16.0 && _bmi <= 16.9) {
+      return 'Severely underweight';
+    } else if (_bmi >= 17.0 && _bmi <= 18.4) {
       return 'Underweight';
+    } else if (_bmi > 18.5 && _bmi <= 24.9) {
+      return 'Normal';
+    } else if (_bmi >= 25.0 && _bmi <= 29.9) {
+      return 'Overweight';
+    } else if (_bmi >= 30.0 && _bmi <= 34.9) {
+      return 'Obesity class 1';
+    } else if (_bmi >=35.0 && _bmi <= 39.9) {
+      return 'Obesity class 2';
+    } else {
+      return 'Obesity class 3';
     }
   }
 
   String interpretation() {
-    if (_bmi > 18.5 && _bmi <= 24.9) {
-      return 'You have a normal body weight. Good job!';
-    } else if (_bmi > 24.9 && _bmi <= 29.9) {
-      return 'You may lose some weight. Ask your Doctor what to do!';
-    } else if (_bmi > 29.9) {
-      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice';
-    } else {
+    if (_bmi < 16) {
+      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice.';
+    } else if (_bmi >= 16.0 && _bmi <= 16.9) {
+      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice.';
+    } else if (_bmi >= 17.0 && _bmi <= 18.4) {
       return 'Your body weight is to low. Ask your Doctor what to do!';
+    } else if (_bmi > 18.5 && _bmi <= 24.9) {
+      return 'You have a normal body weight. Good job!';
+    } else if (_bmi >= 25.0 && _bmi <= 29.9) {
+      return 'You may lose some weight. Ask your Doctor what to do!';
+    } else if (_bmi >= 30.0 && _bmi <= 34.9) {
+      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice.';
+    } else if (_bmi >=35.0 && _bmi <= 39.9) {
+      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice.';
+    } else {
+      return 'Your health may be at risk. Do not wait any longer go to your Doctor for advice.';
     }
   }
 }
+
